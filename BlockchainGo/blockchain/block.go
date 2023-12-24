f6930274d50f85373a33dd9c04ca11c22583630b
ArnavM3434 <arnavnmehta1@gmail.com>
2023-12-24 14:50:22 -0600

    Added proof of work

--- BlockchainGo/blockchain/block.go
+++ BlockchainGo/blockchain/block.go
@@ -1,10 +1,5 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 type BlockChain struct {
 	Blocks []*Block //capitalizing makes it a public field
 }
@@ -13,18 +8,17 @@ type Block struct {
 	Hash     []byte //hash of this block //these are slices
 	Data     []byte
 	PrevHash []byte //last block's hash, allows for linking (like linked list), connects to last block added to blockchain
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{}) //concatenates, makes a new slice
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 
 }
--- BlockchainGo/blockchain/proof.go
+++ BlockchainGo/blockchain/proof.go
@@ -1,6 +1,14 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
 
 //implementing proof of work algorithm
 
@@ -10,7 +18,7 @@ import "math/big"
 
 //create counter (nonce) which starts at 0
 
-//create a hash fo the data plus the counter
+//create a hash for the data plus the counter
 
 //if hash meets set of requirements, we use that hash and sign the block
 
@@ -26,10 +34,71 @@ type ProofOfWork struct {
 
 func NewProof(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
-	target.Lsh(target, uint(256-Difficulty)) //256 bytes in the hash, Lsh left shifts, so you can get the first 12 bits
+	target.Lsh(target, uint(256-Difficulty)) //256 bits in the hash, Lsh left shifts the target
 
 	pow := &ProofOfWork{b, target}
 
 	return pow
+
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+	return data
+
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte //8 * 32 is 256 bits
+
+	nonce := 0
+
+	for nonce < math.MaxInt64 { //almost an infinite loop
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:]) //turn hash into big integer
+
+		if intHash.Cmp(pow.Target) == -1 { //hash is less than target, so we've signed the block
+			break
+
+		} else {
+			nonce++
+
+		}
+
+	}
+	fmt.Println()
+	return nonce, hash[:]
+
+}
+
+func (pow *ProofOfWork) Validate() bool { //after running Run function, you'll have nonce that'll allow you to derive hash that met the target - why can't you just directly use the block's hash instead of running InitData again?
+	var intHash big.Int
+	data := pow.InitData(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num) //writing to the bytes buffer
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return buff.Bytes()
 
 }
